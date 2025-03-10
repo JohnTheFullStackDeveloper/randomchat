c410c33eadc4d0c73ed5bf5bea11dfcbe4b9c921
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2025-03-10 11:06:56 +0530

    success

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3809581381"
+    serviceWorkerVersion: "2178570622"
   }
 });
